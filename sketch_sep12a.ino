0b84743b9449c2ac895cf40b81d13cb6f924ab8e
TintedFireglass <tintedfireglass@hotmail.com>
2021-11-22 22:59:30 +0530

    Add files via upload

---
+++ sketch_sep12a.ino
@@ -0,0 +1,25 @@
+int a[] = {13, 12, 8};
+
+void setup() {
+  pinMode (a[0], OUTPUT);
+  pinMode (a[1], OUTPUT);
+  pinMode (a[2], OUTPUT);
+
+}
+
+void loop() {
+
+  for (int i = 0; i < 3; i++)
+  { digitalWrite(a[i], HIGH);
+    delay(1000);
+    digitalWrite(a[i], LOW) ;
+    delay(1000);
+  }
+
+   digitalWrite(a[1], HIGH);
+    delay(1000);
+    digitalWrite(a[1], LOW) ;
+    delay(1000);
+  
+}
+
